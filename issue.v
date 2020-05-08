c5b6b5455dc5a7d3ea6ee56f765eb3e6a95f1047
jurair bhat <jurairbhat@jurairs-MacBook-Air.local>
2020-05-08 13:41:20 +0530

    changed

--- issue.v
+++ issue.v
@@ -1,31 +1,39 @@
 /* This module gets the instruction from the memory
 puts the data in instruction queue */
 module fetch(
-            input [3:0] pc
+            input reg[3:0] pc
 
   );
-  reg
+//reg [3:0]previous_pc,current_pc;// this will account for stall  condition
+
 always@(posedge clk)
 begin
-  if(run.main.iq_f)
+  if(run.main.iq_f) //and (current_pc != previous_pc))
     begin// it only write in intruction queue if iq_f = 1;
        run.main.instruction_queue[run.main.instruction_queue_ptr] = run.main.instruction_memory[pc];
        run.main.instruction_queue_ptr =  run.main.instruction_queue_ptr + 1'b1;
        if(run.main.instruction_queue_ptr == 4)//4 = length of instruction_queue
-              run.main.iq_r = 0; // instruction_queue_is_full --> dont allow write for further instructionss
+              run.main.iq_r = 0; // instruction_queue_is_full --> dont allow write for further instructions
+       pc = pc + 4'b0001;// next instruction to be fetched;
     end
  else
   $display("Instruction Queue is full ,PC = %b instruction waiting",pc);
 end
 endmodule
 
-/*this module decodes the signal  and add the entry to the ROB;
+/*this module decodes the signal, add the entry to the ROB,
 /and the sends the istruction to appropriate  reservation station */
 module decode(
   input [31:0] instruction,
-  
   output control_signal// assuming 2 functional units for every operation
   );
+always @(posedge clk) begin
+         if(rob_space)
+             begin
+                  run.main.rob_opcode_feild[run.main.tail] = instruction[15:12];// last 4 bits are opcode
+                  run.main.destination_reg_feild[run.main.tail] = instruction[11:8];// destination register
+                  run.main.v_des[run.main.tail] = 0;// making valid bit 0;
+end
 endmodule
 // this implements tomasulos renaming policy
 module rename(
--- run.v
+++ run.v
@@ -1,5 +1,6 @@
 // This the driver module
-// We assume we have 12 bit ISA
+// We assume we have 16 bit ISA.
+// Every entry in data memory is 1 byte ; Hence we perform  1 byte operation;
 // operation : Load 0101, store 0100 , divide 0011 , mul 0010 , add 0001 , sub 0000
 // we have eight registers r1 ------ r16 (0000 ------  10000);
 // we will 4 bits instead of 3 bits to address them for future addition of registes
@@ -21,24 +22,80 @@
 module run ;
 
 //Initializing memory
-reg [11:0]instruction_memory [0:15];//
+reg [15:0]instruction_memory [0:15];//
+
+
+//Initializing register File
+//register_file[0]=R0 and so on--------
+reg [7:0]register_file[0:15]// we have 16 register each 1 byte in size;
+
 
 // This is instruction Queue
-reg [11:0]isntruction_queue[0:3];
+reg [15:0]isntruction_queue[0:3];
 reg iq_f;
 reg [1:0]instruction_queue_ptr;// this points to the next entry in instruction_queue
 
 // ROB
-// ROB size is = 8
+  // opcode des v_des commit
+  // ROB size is = 8
 reg [3:0]rob_opcode_feild[0:7];//ROB isntruction feild
-reg [3:0]destination_reg_feild[0:7]// ROB destination register feild
-reg valid_value[0:7];// 0 = unvalid ,1 = valid ; Ready to retire once it = 1
+reg [3:0]rob_dest_reg_feild[0:7]// ROB destination register feild
+reg [7:0]rob_dest_reg_value[0:3]//destination register value
+reg v_des[0:7];// 0 = unvalid ,1 = valid ; Ready to retire once it = 1
 reg commit[0:7];// indicates whether the particular entry has committed or not
+
 reg [2:0]head;// ROB head
-reg [2:0]tail;// ROB tail
-// Reservation station
-res_add_sub
-res_mul_div
+reg [2:0]tail;// ROB tail// this points to first empty location
+reg rob_space; // this is 0 when ROB is full and 1 when it has entry;
+
+// Reservation station for add and sub = res1
+       // Size of res = 4 instruction;
+reg [3:0]res1_opcode[0:3];
+
+reg [3:0]res1_sr1[0:3]; // sr1 regiter
+reg [7:0]res1_sr1_value[0:3];// sr1 value
+reg res1_v1[0:3];// sr1 valid or not
+
+reg [3:0]res1_sr2[0:3]; // sr2 regiter
+reg [7:0]res1_sr2_value[0:3];// sr2 value
+reg res1_v2[0:3];// sr2 valid or not
+
+reg  [3:0]res1_dest[0:3]; // dest regiter corresponding to ROB;
+reg [7:0]res1_dest_value[0:3];// dest value
+reg res1_v3[0:3]// dest value valid or not
+
+//Reservation station for mul
+    // Size of res_mul = 2 instruction;
+reg [3:0]res2_opcode[0:1];
+
+reg [3:0]res2_sr1[0:1]; // sr1 regiter
+reg [7:0]res2_sr1_value[0:1];// sr1 value
+reg res2_v1[0:1];// sr1 valid or not
+
+reg [3:0]res2_sr2[0:1]; // sr2 regiter
+reg [7:0]res2_sr2_value[0:1];// sr2 value
+reg res2_v2[0:3];// sr2 valid or not
+
+reg  [3:0]res2_dest[0:1]; // dest regiter corresponding to ROB;
+reg [7:0]res2_dest_value[0:1];// dest value
+reg res2_v3[0:1]// dest value valid or not
+
+//Reservation Station for divide
+    // Size of divide = 2 instruction;
+reg [3:0]res3_opcode[0:1];
+
+reg [3:0]res3_sr1[0:1]; // sr1 regiter
+reg [7:0]res3_sr1_value[0:1];// sr1 value
+reg res3_v1[0:1];// sr1 valid or not
+
+reg [3:0]res3_sr2[0:1]; // sr2 regiter
+reg [7:0]res3_sr2_value[0:1];// sr2 value
+reg res3_v2[0:1];// sr2 valid or not
+
+reg  [3:0]res3_dest[0:1]; // dest regiter corresponding to ROB;
+reg [7:0]res3_dest_value[0:1];// dest value
+reg res3_v3[0:1]// dest value valid or not
+
 //
 // LSQ
 reg clk;
